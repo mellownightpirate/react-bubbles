b1306f98da27aa76d6e9eda0d5b5ef46c770fca4
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-02-28 08:26:41 +0000

    made ajax requests to edit/delete data

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import axiosWithAuth from "../auth/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,10 +22,20 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit}`, colorToEdit)
+      .then(()=>{
+        setEditing(false);
+      })
+      .catch((err) => console.log("Error: ", err))
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`/colors/${color.id}`, color)
+    .then((res)=>console.log(res))
+    .catch((err)=> console.log("Error:", err))
   };
 
   return (
